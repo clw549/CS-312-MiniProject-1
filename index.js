eafcdb06c17fab88be6bcbb677db4529b25f406a
Ciaran Wham <clw549@nau.edu>
2025-09-15 08:55:50 -0700

    making improvements

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
-import fs from "fs";
+import fs, { read } from "fs";
 
 const app = express();
 const port = 3000;
@@ -25,3 +25,26 @@ app.post("/", (req,res) => {
   body = req.body;
   console.log(body);
 })
+
+addToJsonFile(JSON.stringify({title:"Hello", content:"World!"}))
+
+function makeJsonFile(data) {
+  fs.writeFileSync("public/posts/posts.json", data);
+}
+
+function addToJsonFile(data) {
+  fs.appendFile("public/posts/posts.json", data, (err)=>{
+    if (err) console.log(err);
+  });
+}
+readJsonFile()
+function readJsonFile() {
+  var data;
+
+  data = fs.readFileSync("public/posts/posts.json","utf-8" , (err, data) => {
+    if (err) console.error(err);
+    else console.log(data); return data;
+  })
+  console.log(data)
+  return data;
+}
